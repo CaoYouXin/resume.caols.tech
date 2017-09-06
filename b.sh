ca65b27a704fb431cd8ed462041be72c8c75ff3e
曹又心 <c_lisheng@163.com>
2017-09-06 15:02:58 +0800

    build script

---
+++ b.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd webapp \
+    && npm run build \
+    && cd .. \
+    # && rm -rf ./docs/ \
+    # && git checkout -- docs/CNAME \
+    # && cp -r ./webapp/build/ ./docs/ \
+    # && cp ./docs/index.html ./docs/404.html
