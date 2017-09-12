933fd77d8fa1390c2c2cf5f0a8e05c1b525d2801
曹又心 <c_lisheng@163.com>
2017-09-12 09:37:34 +0800

    build

--- b.sh
+++ b.sh
@@ -3,7 +3,9 @@
 cd webapp \
     && npm run build \
     && cd .. \
-    # && rm -rf ./docs/ \
+    && rm -rf ./docs/ \
+    && cp -r ./webapp/build/ ./docs/ \
+    && cp ./docs/index.html ./docs/404.html
+
+
     # && git checkout -- docs/CNAME \
-    # && cp -r ./webapp/build/ ./docs/ \
-    # && cp ./docs/index.html ./docs/404.html
